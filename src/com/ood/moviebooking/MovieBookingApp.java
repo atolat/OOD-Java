49f05558bce4b6b1ce95ebf873f1d333dd6865a8
Arpan Sunil Tolat <arpansunil.tolat@broadcom.com>
2021-03-05 12:49:26 -0800

    Update

--- src/com/ood/moviebooking/MovieBookingApp.java
+++ src/com/ood/moviebooking/MovieBookingApp.java
@@ -44,14 +44,14 @@ public class MovieBookingApp {
     public static void main(String[] args) {
         /* --------Data generation code ----START ----------------- */
 
-        // Creating Guest User --> Piyush
-        GuestUser piyush = new GuestUser("Piyush");
+        // Creating Guest User
+        GuestUser guest = new GuestUser("Guest");
 
-        // Creating Registered User --> Ayush
-        RegisteredUser ayush = new RegisteredUser("Ayush");
+        // Creating Registered User
+        RegisteredUser reg_a = new RegisteredUser("User A");
 
         // Creating Registered User --> Saurabh
-        RegisteredUser saurabh = new RegisteredUser("Saurabh");
+        RegisteredUser reg_b = new RegisteredUser("User B");
 
         // Creating Movie object --> Iron Man
         Movie ironMan = new Movie("Iron Man", Language.ENGLISH,Genre.ACTION);
@@ -65,11 +65,11 @@ public class MovieBookingApp {
         // Creating Movie object --> HouseFull2
         Movie housefull = new Movie("HouseFull 2", Language.HINDI,Genre.COMEDY);
 
-        // Creating Theater --> PVR @ GIP Noida with capacity 30
-        Theater pvr_gip = new Theater("PVR","GIP Noida",30);
+        // Creating Theater
+        Theater theater_1 = new Theater("ABC","San Jose",30);
 
-        // Creating Another Theater --> BIG Cinema @ Noida Sector 137 with capacity 40
-        Theater big_cinema = new Theater("Big Cinema","Sector 137 Noida",40);
+        // Creating Another Theater
+        Theater theater_2 = new Theater("XYZ","Fremont",40);
 
 
 
@@ -79,25 +79,25 @@ public class MovieBookingApp {
         SimpleDateFormat formatter = new SimpleDateFormat("EEEE, MMM dd, yyyy HH:mm:ss a");
 
         try {
-            // Creating Show for Movie Iron Man on 7 Jun 2020 @ 9:00 AM in PVR
+            // Creating Show for Movie Iron Man on 7 Jun 2020 @ 9:00 AM
             String dateInString = "Friday, Jun 7, 2020 09:00:00 AM";
             Date date = formatter.parse(dateInString);
-            show1 = new     `Shows(date,ironMan,pvr_gip);
+            show1 = new Shows(date,ironMan,theater_1);
 
-            // Creating Show for Movie HOUSEFULL on 7 Jun 2020 @ 12:00 PM in PVR
+            // Creating Show for Movie HOUSEFULL on 7 Jun 2020 @ 12:00 PM
             dateInString = "Friday, Jun 7, 2020 12:00:00 PM";
             date = formatter.parse(dateInString);
-            show2 = new Shows(date,housefull,pvr_gip);
+            show2 = new Shows(date,housefull,theater_1);
 
-            // Creating Show for Movie WALK TO REMEMBER on 7 Jun 2020 @ 09:00 AM in BIG-CINEMA
+            // Creating Show for Movie WALK TO REMEMBER on 7 Jun 2020 @ 09:00 AM
             dateInString = "Friday, Jun 7, 2020 09:00:00 AM";
             date = formatter.parse(dateInString);
-            show3 = new Shows(date,walkToRemember,big_cinema);
+            show3 = new Shows(date,walkToRemember,theater_2);
 
-            // Creating Show for Movie WALK TO REMEMBER on 7 Jun 2020 @ 12:00 PM in BIG-CINEMA
+            // Creating Show for Movie WALK TO REMEMBER on 7 Jun 2020 @ 12:00 PM
             dateInString = "Friday, Jun 7, 2020 12:00:00 PM";
             date = formatter.parse(dateInString);
-            show4 = new Shows(date,walkToRemember,big_cinema);
+            show4 = new Shows(date,walkToRemember,theater_2);
 
         } catch (ParseException e) {
             e.printStackTrace();
@@ -107,8 +107,8 @@ public class MovieBookingApp {
 
         // Now We have two theaters with their shows, lets add these theaters to our Book My Show app
         ArrayList<Theater> theaterArrayList= new ArrayList<>();
-        theaterArrayList.add(pvr_gip);
-        theaterArrayList.add(big_cinema);
+        theaterArrayList.add(theater_1);
+        theaterArrayList.add(theater_2);
         MovieBookingApp bookMyShow = new MovieBookingApp(theaterArrayList);
 
         // Searching Book My Show for all the shows of movie WALK TO REMEMBER
@@ -120,16 +120,14 @@ public class MovieBookingApp {
         Show{id=4, showTime=Sun Jun 07 12:00:00 IST 2020, movie=The Walk To Remember,
         theater=Big Cinema, availableSeats=40}]
         */
-        // Now suppose AYUSH and SAURABH both wants to book 10 tickets each for first show
-        // Then Book My show will create two Ticket Booking threads for their requests
 
         Shows bookingShow = searchedShow.get(0);
 
         // Ticket Booking Thread for the request made by AYUSH for 10 Seats
-        TicketBookingThread t1 = new TicketBookingThread(bookingShow,ayush,10);
+        TicketBookingThread t1 = new TicketBookingThread(bookingShow,reg_a,10);
 
         // Ticket Booking Thread for the request made by SAURABH for 10 Seats
-        TicketBookingThread t2 = new TicketBookingThread(bookingShow,saurabh,10);
+        TicketBookingThread t2 = new TicketBookingThread(bookingShow,reg_b,10);
 
         // Start both the Ticket Booking Threads for execution
         t1.start();
@@ -144,22 +142,16 @@ public class MovieBookingApp {
             e.printStackTrace();
         }
 
-        // After execution t1 will carry AYUSH ticket and t2 will carry SAURABH ticket
-        Ticket ayush_ticket = t1.getTicket();
-        Ticket saurabh_ticket = t2.getTicket();
+        Ticket ticket1 = t1.getTicket();
+        Ticket ticket2 = t2.getTicket();
 
-        // Printing their tickets
-        System.out.println(t1.getTicket());
-        System.out.println(t2.getTicket());
+        System.out.println(ticket1);
+        System.out.println(ticket2);
 
         // Now, 20 seats are booked for this show and 20 seats are available,
-        // Suppose AYUSH wants another 15 seats and SAURABH wants another 10 seats to be booked
 
-        // Ticket Booking Thread for the request made by AYUSH for another 15 Seats
-        TicketBookingThread t3 = new TicketBookingThread(bookingShow,ayush,15);
-
-        // Ticket Booking Thread for the request made by SAURABH for another 10 Seats
-        TicketBookingThread t4 = new TicketBookingThread(bookingShow,saurabh,10);
+        TicketBookingThread t3 = new TicketBookingThread(bookingShow,reg_a,15);
+        TicketBookingThread t4 = new TicketBookingThread(bookingShow,reg_b,10);
 
         // Start both the Ticket Booking Threads for execution
         t3.start();
@@ -175,12 +167,11 @@ public class MovieBookingApp {
             e.printStackTrace();
         }
 
-        // After execution t3 will carry AYUSH ticket and t4 will carry SAURABH ticket
-        Ticket ayushNewTicket = t3.getTicket();
-        Ticket saurabhNewTicket = t4.getTicket();
+        Ticket ticket3 = t3.getTicket();
+        Ticket ticket4 = t4.getTicket();
 
-        System.out.println(ayushNewTicket);
-        System.out.println(saurabhNewTicket);
+        System.out.println(ticket3);
+        System.out.println(ticket4);
 
         /* Running this program several times, we will notice that
         if t3 enters the bookTicket function first,
