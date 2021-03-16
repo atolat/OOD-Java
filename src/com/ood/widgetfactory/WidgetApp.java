13ff273f4b29995b7a3ccb10255e5d642c417d55
atolat <arpantolat30@gmail.com>
2021-03-16 11:46:14 -0700

    Update

---
+++ src/com/ood/widgetfactory/WidgetApp.java
@@ -0,0 +1,82 @@
+package com.ood.widgetfactory;
+/*
+ This class demonstrates the use of the decorator pattern
+ Requirements:
+- A widget factory that draws shapes - circle, triangle, square, etc.
+- We want to support a canvas app that uses the widget factory to draw shapes with attributes like
+    - color
+    - shade
+    - etc.
+*/
+interface Shape {
+    String draw();
+}
+
+class Circle implements Shape{
+    @Override
+    public String draw() {
+        return "Circle";
+    }
+}
+
+class Triangle implements Shape{
+    @Override
+    public String draw() {
+        return "Triangle";
+    }
+}
+
+
+class ShapeDecorator implements Shape {
+    Shape shape;
+
+    public ShapeDecorator(Shape shape) {
+        this.shape = shape;
+    }
+
+    @Override
+    public String draw() {
+        return null;
+    }
+}
+
+class ColorDecorator extends ShapeDecorator {
+
+    String color;
+
+    public ColorDecorator(String color, Shape shape) {
+        super(shape);
+        this.color = color;
+    }
+
+    @Override
+    public String draw() {
+        return this.color + " " + this.shape.draw();
+    }
+}
+
+class ShadeDecorator extends ShapeDecorator {
+
+    String shade;
+
+    public ShadeDecorator(String shade, Shape shape) {
+        super(shape);
+        this.shade = shade;
+    }
+
+    @Override
+    public String draw() {
+        return this.shade + " " + this.shape.draw();
+    }
+}
+
+public class WidgetApp {
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        System.out.println(circle.draw());
+        Shape coloredCircle = new ShadeDecorator("Red", circle);
+        System.out.println(coloredCircle.draw());
+        Shape shadedColoredCircle = new ShadeDecorator("Dotted", coloredCircle);
+        System.out.println(shadedColoredCircle.draw());
+    }
+}
